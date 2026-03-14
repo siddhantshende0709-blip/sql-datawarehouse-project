debda665f0e7f0dcd9df0a613ca601ca71cd4797
siddhantshende0709-blip <siddhantshende0709@gmail.com>
2026-03-14 22:11:45 +0530

    Update proc_load__silver.sql

--- scripts/silver/proc_load__silver.sql
+++ scripts/silver/proc_load__silver.sql
@@ -191,8 +191,8 @@ BEGIN
         CASE WHEN bdate > GETDATE() THEN NULL
             ELSE bdate
         END AS bdate,
-        CASE WHEN UPPER(TRIM(gen)) IN ('F','FEMALE') THEN ' Female'
-            WHEN UPPER(TRIM(gen)) IN ('M','MALE') THEN ' Male'
+        CASE WHEN UPPER(TRIM(gen)) IN ('F','FEMALE') THEN 'Female'
+            WHEN UPPER(TRIM(gen)) IN ('M','MALE') THEN 'Male'
             ELSE 'n/a'
         END AS gen
         FROM bronze.erp_cust_az12
